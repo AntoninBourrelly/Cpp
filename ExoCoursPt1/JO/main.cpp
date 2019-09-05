24a3e1b2eb521758951d6be6ca74923156befd98
abourelly <antonin.bourrelly.touchard@gmail.com>
2019-09-05 10:25:51 +0200

    maj references

--- ExoCoursPt1/JO/main.cpp
+++ ExoCoursPt1/JO/main.cpp
@@ -9,6 +9,7 @@ int main(){
     cin >> nomDuFichier;
     //Création du flux en lecture sur le fichier
     ifstream leFichier(nomDuFichier.c_str());
+    ofstream exportFichier("export.txt");
     //c_str() transforme string en char*
     if(!leFichier .is_open())
         cerr << "Erreur lors de l'ouverture du fichier"<<endl;
@@ -17,18 +18,19 @@ int main(){
         int nbOr;
         int nbArgent;
         int nbBronze;
-        cout<< "+" << setfill('-') << setw(18) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< endl;
+        exportFichier<< "+" << setfill('-') << setw(18) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< endl;
         do{
             //récupération des valeurs
             leFichier >> pays >> nbOr >> nbArgent >> nbBronze;
             if(leFichier.good()) //Si les valeur sont bien été lues
             {
-                cout << "|" << setw(17) << left << pays << right << "|" << setw(8) << nbOr << "|" << setw(8) << nbArgent << "|" << setw(8) << nbBronze << "|" << endl;
+                exportFichier << "|" << setw(17) << left << pays << right << "|" << setw(8) << nbOr << "|" << setw(8) << nbArgent << "|" << setw(8) << nbBronze << "|" << endl;
             }
         }while(!leFichier.eof());
-        cout<< "+" << setfill('-') << setw(18) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" << setfill(' ')<< endl;
+        exportFichier<< "+" << setfill('-') << setw(18) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" <<setfill(' ')<< setfill('-') << setw(9) << "+" << setfill(' ')<< endl;
 
 
     }
+    exportFichier.close();
     return 0;
 }
